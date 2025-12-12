9bd4ffafba5dec3fb9dc97ab4b760b0f2fc16406
rozer <shubhamsinha07084@gmail.com>
2025-12-12 18:51:50 +0530

    setting up better-auth & mongodb database

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -1,37 +1,37 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 declare global {
-    var mongoosecache: {
+    var mongooseCache: {
         conn: typeof mongoose | null;
-        promise: Promise<typeof mongoose> | null; // type of promise -> Promise
+        promise: Promise<typeof mongoose> | null;
     }
 }
 
-let cached = global.mongoosecache;
+let cached = global.mongooseCache;
 
 if(!cached) {
-    cached = global.mongoosecache = { conn: null, promise: null};
+    cached = global.mongooseCache = { conn: null, promise: null };
 }
 
 export const connectToDatabase = async () => {
-    if(!MONGODB_URI) throw new Error("MONGODB_URI must be set within .env")
+    if(!MONGODB_URI) throw new Error('MONGODB_URI must be set within .env');
 
-    if(!cached.conn) return cached.conn;
+    if(cached.conn) return cached.conn;
 
     if(!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, {bufferCommands: false});
+        cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
     }
 
     try {
         cached.conn = await cached.promise;
-        
-    } catch (error) {
+    } catch (err) {
         cached.promise = null;
-        throw error;
+        throw err;
     }
 
-    console.log(`Conneced to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
 
+    return cached.conn;
 }

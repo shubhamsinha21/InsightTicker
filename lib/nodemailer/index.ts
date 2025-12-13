1f1753893e297e5241d1c4a70fcf91343cf4873a
rozer <shubhamsinha07084@gmail.com>
2025-12-13 16:39:36 +0530

    added helper function in nodemailer

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,4 +1,5 @@
 import nodemailer from "nodemailer";
+import { WELCOME_EMAIL_TEMPLATE } from "./template";
 
 export const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -8,3 +9,22 @@ export const transporter = nodemailer.createTransport({
 
     }
 })
+
+// helper function to send the email using transporter
+
+export const sendWelcomeEmail = async ({email, name, intro}: WelcomeEmailData) => {
+    const htmlTemplate = WELCOME_EMAIL_TEMPLATE
+    .replace("{{name}}", name)
+    .replace("{{intro}}", intro);
+
+    const mailOptions = {
+        from: `"InsightTicker" <insightticker@gmail.com>`,
+        to: email,
+        subject: `Welcome to InsightTicker - your stock market toolkit is ready!`,
+        text: "Thanks for joining InsightTicker",
+        html: htmlTemplate,
+    }
+
+    await transporter.sendMail(mailOptions)
+
+}
